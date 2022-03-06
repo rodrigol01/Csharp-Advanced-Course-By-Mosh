f999b112c8a59b6042259d755fcd7af08223542f
Rodrigo Leonardo <rodrigoleonardo81@gmail.com>
2022-03-06 14:53:49 -0300

    first example of generics (or something nothing generic xD)

---
+++ AdvancedTopicsCsharp/Generics/ListOfAnything.cs
@@ -0,0 +1,17 @@
+﻿namespace Generics;
+
+public class ListOfAnything
+{
+    //this first code is a limited code and unproductively
+    //this class is just limited to type int, if I want to
+    //add other types I will need create other classes...
+    
+    public List<int> Values { get; set; }
+
+    public void Add(int value)
+    {
+        Values.Add(value);
+    }
+
+    public int this[int index] => throw new NotImplementedException();
+}
