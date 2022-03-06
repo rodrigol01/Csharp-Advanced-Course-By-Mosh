269af3c421544c8b4f60a757079dc9827dcf60cc
Rodrigo Leonardo <rodrigoleonardo81@gmail.com>
2022-03-06 15:41:43 -0300

    using dictionary

--- AdvancedTopicsCsharp/Generics/ListOfAnything.cs
+++ AdvancedTopicsCsharp/Generics/ListOfAnything.cs
@@ -13,3 +13,15 @@ public class ListOfAnything<T>
 
     public T this[int index] => throw new NotImplementedException();
 }
+
+public class GenericDictionary<Tkey, TValue> where Tkey : notnull
+{
+    //now, we can play with generic classes from .NET
+    //like Dictionary
+    public Dictionary<Tkey, TValue> Values { get; set; }
+
+    public void Add(Tkey key, TValue value)
+    {
+        Values.Add(key, value);
+    }
+}
--- AdvancedTopicsCsharp/Generics/Program.cs
+++ AdvancedTopicsCsharp/Generics/Program.cs
@@ -10,3 +10,6 @@ listOfStrings.Add("Hi, I'm generic, performative, reused and productive");
 
 var listOfBooks = new ListOfAnything<Book>();
 listOfBooks.Add(new Book("The Little Prince", 1943));
+
+var dictionary = new GenericDictionary<string, Book>();
+dictionary.Add("children's literature", new Book("the little prince", 1943));
