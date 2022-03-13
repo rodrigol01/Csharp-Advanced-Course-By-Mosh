278921a6a0344523548dac6df6299f945380b5ae
Rodrigo Leonardo <rodrigoleonardo81@gmail.com>
2022-03-13 12:54:54 -0300

    in previously class Utilities, it didn't generic, now we transform him in generic class

--- AdvancedTopicsCsharp/Generics/Utilities.cs
+++ AdvancedTopicsCsharp/Generics/Utilities.cs
@@ -1,13 +1,12 @@
-﻿public class Utilities
+﻿public class Utilities<T> where T : IComparable
 {
-    //the class isn't generic but the MaxGeneric is generic
-    //it's not a problem
+    //but now, we will transform the class in generic.
     
     //not generic
     public int Max (int a, int b) =>
          a > b ? a : b;
     
     //generic with the constraint to IComparable
-    public T MaxGeneric <T>(T a, T b) where T : IComparable =>
+    public T MaxGeneric(T a, T b) =>
         a.CompareTo(b) > 0 ? a : b;
 }
