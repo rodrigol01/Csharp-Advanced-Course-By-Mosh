b07b9d3c05487079ad08dbcff844a0ad13ecc6ef
Rodrigo Leonardo <rodrigoleonardo81@gmail.com>
2022-03-13 12:52:27 -0300

    add a generic method in class utilities that compare 2 values and return the max value

---
+++ AdvancedTopicsCsharp/Generics/Utilities.cs
@@ -0,0 +1,13 @@
+﻿public class Utilities
+{
+    //the class isn't generic but the MaxGeneric is generic
+    //it's not a problem
+    
+    //not generic
+    public int Max (int a, int b) =>
+         a > b ? a : b;
+    
+    //generic with the constraint to IComparable
+    public T MaxGeneric <T>(T a, T b) where T : IComparable =>
+        a.CompareTo(b) > 0 ? a : b;
+}
