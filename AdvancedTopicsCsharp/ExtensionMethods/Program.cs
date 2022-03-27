e84ba14a27df222deb4f81f8cbf12fdf22d75745
Rodrigo Leonardo <rodrigoleonardo81@gmail.com>
2022-03-27 20:46:37 -0300

    extension methods

---
+++ AdvancedTopicsCsharp/ExtensionMethods/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+//allow us to add methods to an existing class
+//without changing its source code, or creating
+// a new class that inhetis from it
+
+string post = "This is supposed to be a very long blog post blah blah blah...";
+
+var shortenedPost = post.Shorten(5);
+
+Console.WriteLine(shortenedPost);
---
+++ AdvancedTopicsCsharp/ExtensionMethods/StringExtensions.cs
@@ -0,0 +1,24 @@
+﻿namespace System
+{
+    //create a public static class "type + extensions"
+
+    public static class StringExtensions
+    {
+        public static string Shorten(this String str, int numberOfWords)
+        {
+            if (numberOfWords < 0)
+                throw new ArgumentOutOfRangeException("number of words should be greater than or equal to 0");
+
+            if (numberOfWords == 0)
+                return "";
+
+            var words = str.Split(' ');
+
+            if (words.Length <= numberOfWords)
+                return str;
+
+            return string.Join(" ", words.Take(numberOfWords)) + "...";
+        }
+    }
+}
+
